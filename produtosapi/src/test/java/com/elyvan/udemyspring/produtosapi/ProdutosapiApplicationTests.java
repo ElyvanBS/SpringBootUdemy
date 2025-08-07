6b35ea628ef4226223c11e754cb7b518cae16035
Elyvanbs <elyvanbs@gmail.com>
2025-08-07 16:52:49 -0500

    feat: Adiciona os dados iniciais do projeto produtosapi do Curso da Udemy de Spring

---
+++ produtosapi/src/test/java/com/elyvan/udemyspring/produtosapi/ProdutosapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elyvan.udemyspring.produtosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutosapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
